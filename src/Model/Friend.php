2b412041f7698edb1bcafed674ed55006b233fd4
blestab <blestab@users.noreply.github.com>
2019-05-10 21:46:30 +0200

    feat: Friend - Added aggregates and an expression
    total_load, total_repayment, and owed all of type money

--- src/Model/Friend.php
+++ src/Model/Friend.php
@@ -12,5 +12,13 @@ class Friend extends \atk4\data\Model {
     ]);
 
     $this->hasOne('user_id', new User());
+
+    $ref = $this->hasMany('Loans', new Loan());
+    $ref->addField('total_loan', ['aggregate'=>'sum', 'field'=>'amount', 'type'=>'money']);
+
+    $ref = $this->hasMany('Repayments', new Repayment());
+    $ref->addField('total_repayment', ['aggregate'=>'sum', 'field'=>'amount', type=>'money']);
+
+    $this->addExpression('owed', ['[total_loan]-[total_repayment]', type=>'money']);
   }
 }
